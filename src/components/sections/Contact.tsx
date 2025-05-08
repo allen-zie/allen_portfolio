7f759c18a0c254b25200f71c90b5b5007008b5f8
zipho.allen21 <allenziphondlovu@outlook.com>
2025-05-08 03:59:00 +0200

    Emailing

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -22,8 +22,8 @@ export const Contact = () => {
     
     if (formRef.current) {
       emailjs.sendForm(
-        'service_gesrcxn', // Replace with your EmailJS service ID
-        'template_m5vk4fe', // Replace with your EmailJS template ID
+        'gesrcxn', // Replace with your EmailJS service ID
+        'm5vk4fe', // Replace with your EmailJS template ID
         formRef.current,
         'e4I6NSI6-2SwYwmH5' // Replace with your EmailJS public key
       )
