eb2e4a3c9c69b4db6ce6b9a6d109f0ffc5e7801c
allen-zie <allenziphondlovu@outlook.com>
2025-05-17 22:03:11 +0200

    About Edit

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -17,7 +17,20 @@ export const About = () => {
             scalable applications that deliver exceptional user experiences.
           </p>
           <p className="animate-fadeIn" style={{ animationDelay: "600ms" }}>
-            When I'm not coding, you can find me exploring new technologies, Shoouting at TV screens hoping to see Lewis Hamilton become an 8 time world champion, or watch grown men trying to kill each other on the field in what americans call Football but we call it American Football (NFL) or sharing my knowledge with the developer community.
+            In Network Engineering and Design I focus on designing, implementing, and optimizing 
+            robust network infrastructures. I specialize in enterprise networking, cloud networking, 
+            security. I and am passionate about creating efficient, scalable, and secure solutions and
+            optimizing network performance while reducing operational costs. 
+          </p>
+          <p className="animate-fadeIn" style={{ animationDelay: "800ms" }}>
+            When I'm not coding, you'll find me exploring new technologies, shouting at the telly 
+            hoping to see Lewis Hamilton become an eighth world champion, or watching grown men 
+            clatter into each other on the pitch in what Americans call football but we call American 
+            football (NFL), or sightseeing and travelling.
+          </p>
+          <p className="animate-fadeIn" style={{ animationDelay: "1000ms" }}><b><i>
+            "Coding solves problems, but sports and travel fuel my creativity and keep me balanced."
+          </i></b>
           </p>
         </div>
       </div>
